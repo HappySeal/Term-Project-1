f21a79126769569e76c79812756420ea30b9ca09
Cafer SELLI <cafer15165@gmail.com>
2022-05-21 01:54:10 +0300

    Scenario 1

--- main.m
+++ main.m
@@ -40,54 +40,71 @@ t = 0;
 while t < 120
     % MOVEMENT PHASE
     for i = 1:N
-        minBound = [1,1];
-        maxBound = [T,T];
-        if PERSON(i,4) > 0
-            minBound = PERSON(i,[5,6]) - 1;
-            minBound(1) = max(1,minBound(1));
-            minBound(2) = max(1,minBound(2));
-            
-            maxBound = PERSON(i,[5,6]) + 1;
-            maxBound(1) = min(T,maxBound(1));
-            maxBound(2) = min(T,maxBound(2));
-        end
-
-        index = randi(8);
-        while any(PERSON(i,[1,2])+dir(index,:)>maxBound) || any(PERSON(i,[1,2])+dir(index,:)<minBound)
-            index=randi(8);
-        end
-        movementVector = dir(index,:) * randi([0 3]);
-        while any(PERSON(i,[1,2])+movementVector>maxBound) || any(PERSON(i,[1,2])+movementVector<minBound)
+        if ~PERSON(i,7)
+            minBound = [1,1];
+            maxBound = [T,T];
+            if PERSON(i,4) > 0
+                minBound = PERSON(i,[5,6]) - 1;
+                minBound(1) = max(1,minBound(1));
+                minBound(2) = max(1,minBound(2));
+                
+                maxBound = PERSON(i,[5,6]) + 1;
+                maxBound(1) = min(T,maxBound(1));
+                maxBound(2) = min(T,maxBound(2));
+            end
+    
+            index = randi(8);
+            while any(PERSON(i,[1,2])+dir(index,:)>maxBound) || any(PERSON(i,[1,2])+dir(index,:)<minBound)
+                index=randi(8);
+            end
             movementVector = dir(index,:) * randi([0 3]);
+            while any(PERSON(i,[1,2])+movementVector>maxBound) || any(PERSON(i,[1,2])+movementVector<minBound)
+                movementVector = dir(index,:) * randi([0 3]);
+            end
+            PERSON(i,[1,2]) = PERSON(i,[1,2]) + movementVector;
         end
-        PERSON(i,[1,2]) = PERSON(i,[1,2]) + movementVector;
-
-        % TODO: If the movement is greater than the boundry of the grid
-        % make sure that it is capped at max position
-
-
     end
     % ITERATION PHASE
-    for i = 1:N
-        % TODO: make iterations go up/down through the iters.
 
+    for i = 1:N
+        if ~PERSON(i,7)
+            if PERSON(i,3) > 0
+                PERSON(i,3) = PERSON(i,3) - 1;
+                if PERSON(i,4) > 0
+                    PERSON(i,4) = PERSON(i,4) - 1;
+                end
+    
+                if(PERSON(i,3) == 0)
+                    if(rand < 0.95)
+                        PERSON(i,8) = 1;
+                    else
+                        PERSON(i,7) = 1;
+                    end
+                end
+            end
+        end
     end
 
 
 
     % INFECT PHASE
     for i = 1:N
-        for j = i+1:N
-            if((PERSON(i,3) > 0)  ~= (PERSON(j,3) > 0))
-                if(PERSON(i,3) > PERSON(j,3))
-                    index = j;
-                else
-                    index = i;
+        if ~PERSON(i,7)
+            for j = i+1:N
+                if ~PERSON(j,7)
+                    if((PERSON(i,3) > 0)  ~= (PERSON(j,3) > 0))
+                        if(PERSON(i,3) > PERSON(j,3))
+                            index = j;
+                        else
+                            index = i;
+                        end
+                        infected = PERSON(index,:);
+                        infectedProbability = (~PERSON(index,8)) && (rand < p);
+                        isolatedProbability = infectedProbability && (rand < qS);
+                        PERSON(index,[3,4]) = [M*infectedProbability, M*isolatedProbability];
+                        PERSON(index,[5,6]) = PERSON(index,[1,2]) * isolatedProbability;
+                    end
                 end
-                infected = PERSON(index,:);
-                infectedProbability = (~PERSON(index,8)) && (rand < p);
-                isolatedProbability = infectedProbability && (rand < qS);
-                PERSON(index,[3,4]) = [M*infectedProbability, M*isolatedProbability];
             end
         end
     end
