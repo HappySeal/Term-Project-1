28d8e30bf49dff434302d4a946e00fd1d8ada6c7
Cafer SELLI <cafer15165@gmail.com>
2022-05-22 02:29:49 +0300

    Scenario III Implemented

--- Scenario III/ScenarioIII.m
+++ Scenario III/ScenarioIII.m
@@ -1,24 +1,33 @@
-function [newPERSON,histInfected,histHealed,histDied,histVaccinated] = ScenarioII(PERSON,N,T,M,p,rS,tS)  
+function [newPERSON,histInfected,histHealed,histDied,histVaccinated] = ScenarioIII(PERSON,N,T,M,qS,p,rS,tS)  
     % CONSTANTS
     dir = [0,1;1,1;1,0;1,-1;0,-1;-1,-1;-1,0;-1,1];
 
     histInfected = zeros([1,120]);
-    histVaccinated = zeros([1,120]);
     histHealed = zeros([1,120]);
     histDied = zeros([1,120]);
+    histVaccinated = zeros([1,120]);
 
     t = 1;
     while t <= 120
         histInfected(t) = sum(PERSON(:,3) > 0);
-        histHealed(t) = sum(PERSON(:,5) > 0);
-        histDied(t) = sum(PERSON(:,4) > 0);
-        histVaccinated(t) = sum(PERSON(:,6) > 0);
+        histHealed(t) = sum(PERSON(:,8) > 0);
+        histDied(t) = sum(PERSON(:,7) > 0);
+        histVaccinated(t) = sum(PERSON(:,9) > 0);
 
         % MOVEMENT PHASE
         for i = 1:N
-            if ~PERSON(i,4)
+            if ~PERSON(i,7)
                 minBound = [1,1];
                 maxBound = [T,T];
+                if PERSON(i,4) > 0
+                    minBound = PERSON(i,[5,6]) - 1;
+                    minBound(1) = max(1,minBound(1));
+                    minBound(2) = max(1,minBound(2));
+                    
+                    maxBound = PERSON(i,[5,6]) + 1;
+                    maxBound(1) = min(T,maxBound(1));
+                    maxBound(2) = min(T,maxBound(2));
+                end
         
                 index = randi(8);
                 while any(PERSON(i,[1,2])+dir(index,:)>maxBound) || any(PERSON(i,[1,2])+dir(index,:)<minBound)
@@ -47,33 +56,65 @@ function [newPERSON,histInfected,histHealed,histDied,histVaccinated] = ScenarioI
                 end
                 
                 maxMag = min(angles);
+                
 
-                PERSON(i,[1,2]) = PERSON(i,[1,2]) + dir(index,:) * maxMag;
+                movementVector = dir(index,:) * min(maxMag,randi([0 3]));
+                PERSON(i,[1,2]) = PERSON(i,[1,2]) + movementVector;
             end
         end
         % ITERATION PHASE
     
         for i = 1:N
-            if ~PERSON(i,4)
+            if ~PERSON(i,7)
                 if PERSON(i,3) > 0
                     PERSON(i,3) = PERSON(i,3) - 1;
+                    if PERSON(i,4) > 0
+                        PERSON(i,4) = PERSON(i,4) - 1;
+                    end
         
                     if(PERSON(i,3) == 0)
                         if(rand < 0.95)
-                            PERSON(i,5) = 1;
+                            PERSON(i,8) = 1;
                         else
-                            PERSON(i,4) = 1;
+                            PERSON(i,7) = 1;
                         end
                     end
                 end
             end
         end
-
+    
+        % INFECT PHASE
+        for i = 1:N
+            if ~PERSON(i,7)
+                for j = i+1:N
+                    if ~PERSON(j,7)
+                        if((PERSON(i,3) > 0)  ~= (PERSON(j,3) > 0))
+                            if(all(PERSON(i,[1,2]) == PERSON(j,[1,2])))
+                                if(PERSON(i,3) > PERSON(j,3))
+                                    index = j;
+                                else
+                                    index = i;
+                                end
+                                if ~PERSON(index,9)
+                                    infectedProbability = (~PERSON(index,8)) && (rand < p);
+                                else
+                                    infectedProbability = (~PERSON(index,8)) && (rand < rS);
+                                end
+                                isolatedProbability = infectedProbability && (rand < qS);
+                                PERSON(index,[3,4]) = [M*infectedProbability, M*isolatedProbability];
+                                PERSON(index,[5,6]) = PERSON(index,[1,2]) * isolatedProbability;
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    
         % VACCINATION PHASE
         if t >= tS
-            healthyPEOPLE = find(all(PERSON(:,[3,4,5,6]) == 0,2));
+            healthyPEOPLE = find(all(PERSON(:,[3,7,8,9]) == 0,2));
             n = length(healthyPEOPLE);
-            disp(n);
+            
             if n > 0
                 delta3 = 1 / (2 * (t - 19));
                 numOfVaccinated = delta3 * n;
@@ -91,37 +132,13 @@ function [newPERSON,histInfected,histHealed,histDied,histVaccinated] = ScenarioI
                         index = randi(n);
                     end
                 
-                    PERSON(healthyPEOPLE(index),6) = 1;
+                    PERSON(healthyPEOPLE(index),9) = 1;
                     indexVaccinated(i) = index;
                 end
             end
         end
     
-        % INFECT PHASE
-        for i = 1:N
-            if ~PERSON(i,4)
-                for j = i+1:N
-                    if ~PERSON(j,4)
-                        if((PERSON(i,3) > 0)  ~= (PERSON(j,3) > 0))
-                            if(all(PERSON(i,[1,2]) == PERSON(j,[1,2])))
-                                if(PERSON(i,3) > PERSON(j,3))
-                                    index = j;
-                                else
-                                    index = i;
-                                end
-                                if ~PERSON(index,6)
-                                    infectedProbability = (~PERSON(index,5)) && (rand < p);
-                                else
-                                    infectedProbability = (~PERSON(index,5)) && (rand < rS);
-                                end
-                                PERSON(index,3) = M*infectedProbability;
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    
+
         t = t + 1;
     end
     newPERSON = PERSON;
